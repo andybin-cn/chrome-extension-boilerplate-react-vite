b1c21696bc9b06b54b2ec3b2ba3677a93bd2ceec
andy.bin <andybin.cn@gmail.com>
2023-05-22 17:41:15 +0800

    feat: block full reload

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,6 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.js'
 import './index.css'
 
+if (import.meta.hot) {
+  // avoid the page reload when change the code at `@beat-wallet/server` or other etc.
+  import.meta.hot.on('vite:beforeFullReload', () => {
+    throw 'skipping full reload, please refresh manually'
+  })
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
